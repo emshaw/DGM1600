71bf7a6e0291b004ce5f6d4aab620aaceec2de50
Unknown <emillyshaw@gmail.com>
2017-01-23 10:43:48 -0700

    Added changeColor script

---
+++ MyFirstProject/Assets/changeColor.cs
@@ -0,0 +1,10 @@
+using UnityEngine:
+
+public class changeColor : MonoBehaviour {
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.R))
+			GetComponent<Renderer>().material.color = Color.red;
+
+	}
+
+}
