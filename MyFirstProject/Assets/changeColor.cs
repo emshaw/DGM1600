34eb19d94cacc78371256e07b295c873b57321de
Unknown <emillyshaw@gmail.com>
2017-01-25 10:39:43 -0700

    added black, white, yellow colors

--- MyFirstProject/Assets/changeColor.cs
+++ MyFirstProject/Assets/changeColor.cs
@@ -8,6 +8,12 @@ public class changeColor : MonoBehaviour {
 			GetComponent<Renderer>().material.color = Color.blue;
 	if (Input.GetKeyDown(KeyCode.G))
 			GetComponent<Renderer>().material.color = Color.green;
+	if (Input.GetKeyDown(KeyCode.Delete))
+			GetComponent<Renderer>().material.color = Color.white;
+	if (Input.GetKeyDown(KeyCode.Space))
+			GetComponent<Renderer>().material.color = Color.black;
+	if (Input.GetKeyDown(KeyCode.Return))
+			GetComponent<Renderer>().material.color = Color.yellow;
 
 
 	
