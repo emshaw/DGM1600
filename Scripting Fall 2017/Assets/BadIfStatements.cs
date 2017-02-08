1d0784508707fac1f6a46799abec69e7c04b5b85
Unknown <emillyshaw@gmail.com>
2017-02-08 10:46:25 -0700

    added if and else statements

---
+++ Scripting Fall 2017/Assets/BadIfStatements.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadIfStatements : MonoBehaviour {
+
+
+    public string animal = "Cat";
+    
+    // Use this for initialization
+	void Start () {
+        //Copy and pasting code is BAD!!! code should not all look the same
+        //This code is technically not wrong, it's just super annoying - bad form
+        if (animal == "Cat")
+        {
+            print("Cat");
+        }
+
+        if (animal == "Dog")
+        {
+            print("Dog");
+        }
+
+        if (animal == "Bird")
+        {
+            print("Bird");
+        }
+
+        if (animal == "Elephant")
+        {
+            print("Elephant");
+        }
+
+        if (animal == "Mole")
+        {
+            print("Mole");
+        }
+    }
+	
+	
+}
