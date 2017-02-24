c0cd4c3025ddc25d1ae1b4a5b5daf711c344fe49
Unknown <emillyshaw@gmail.com>
2017-02-24 11:18:45 -0700

    Added More Script

---
+++ Scripting Fall 2017/Assets/ForLoops.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ForLoops : MonoBehaviour {
+
+    public List<string> cars;
+    
+    
+    // Use this for initialization
+	void Start () {
+        //a for loop has an iterator - an iterator is used to count - usually started at 0
+        for (int i = 0; i < cars.Count; i++)//i++ adds one because ++ means add 1 - for lists, it's ____.count, for arrays, it's ____.length
+        {
+            print(cars[i]);//use i because that's the indexer that is being used earlier
+        }
+    }
+}
---
+++ Scripting Fall 2017/Assets/Problem3.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//PROBLEM: Create 3 Buttons, Add New Script to each button, Write a function (void) that adds an element to the myElements list (animals), Configure the button to run the function that adds the element to the list
+
+public class Problem3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.A))
+        {
+            print("Aardvark");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("Sloth");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Dingo");
+        }
+        
+	}
+}
